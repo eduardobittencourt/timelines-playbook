9527e2afecf41338c92ffc2713bdca110e506137
Eduardo Bittencourt <eduardo.oliveira@nodo.cc>
2021-03-29 21:36:51 -0300

    add spreadsheet

---
+++ libs/sheets.js
@@ -0,0 +1,24 @@
+import { google } from 'googleapis';
+export async function getColumns() {
+  try {
+    const target = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+    const jwt = new google.auth.JWT(
+      process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+      null,
+      (process.env.GOOGLE_SHEETS_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+      target
+    );
+
+    const sheets = google.sheets({ version: 'v4', auth: jwt });
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.SPREADSHEET_ID,
+      range: 'config',
+    });
+
+    const rows = response.data.values;
+    return rows
+  } catch (err) {
+    console.log(err);
+  }
+  return [];
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,142 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState } from 'react'
+import { AppBar, Container, Toolbar, Typography, Button, TextField, Select, MenuItem, Grid, FormControl, FormLabel, Paper } from '@material-ui/core'
+import { useForm, Controller } from "react-hook-form"
+import { format, addBusinessDays } from 'date-fns'
+import { getColumns } from '../libs/sheets';
+import {
+  KeyboardDatePicker,
+  MuiPickersUtilsProvider
+} from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
+import { makeStyles } from '@material-ui/core/styles';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default function IndexPage({ data }) {
+  const [name, setName] = useState(null)
+  const [dates, setDates] = useState(null)
+  const { handleSubmit, control } = useForm()
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+  const onSubmit = handleSubmit(values => {
+    const { date, project } = values
+    const { name, ...baseProject } = data[project]
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+    const result = Object.entries(baseProject).reduce((acc, [key, value]) => {
+      const newDate = format(addBusinessDays(date, value), 'dd/MM/yyyy')
+      return { ...acc, [key]: newDate }
+    }, {})
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+    setName(values.name)
+    setDates(result)
+  })
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+  const classes = useStyles()
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+  return <>
+    <AppBar position="sticky" className={classes.appBar}>
+      <Toolbar>
+        <Typography variant="h6">
+          Pravy
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <Container>
+      <form onSubmit={onSubmit}>
+        <Paper  className={classes.paper}>
+            <Grid container spacing={3} alignItems='flex-end'>
+              <Grid item xs={4}>
+                <FormControl fullWidth>
+                  <FormLabel>Name</FormLabel>
+                  <Controller as={TextField} name="name" control={control} fullWidth />
+                </FormControl>
+              </Grid>
+              <Grid item xs={4}>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                  <Controller
+                    name="date"
+                    control={control}
+                    render={({ ref, ...rest }) => (
+                      <KeyboardDatePicker
+                        fullWidth
+                        margin="none"
+                        id="date-picker-dialog"
+                        label="Kickoff"
+                        format="dd/MM/yyyy"
+                        KeyboardButtonProps={{
+                          "aria-label": "change date"
+                        }}
+                        {...rest}
+                      />
+                    )}
+                  />
+                </MuiPickersUtilsProvider>
+              </Grid>
+              <Grid item xs={4}>
+                <FormControl fullWidth>
+                  <FormLabel>Project Type</FormLabel>
+                  <Controller
+                    as={
+                      <Select fullWidth>
+                        {Object.keys(data).map(value => <MenuItem key={value} value={value}>{value}</MenuItem>)}
+                      </Select>
+                    }
+                    name="project"
+                    control={control}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <Button variant='contained' color='primary' type="submit">Submit</Button>
+              </Grid>
+            </Grid>
+        </Paper>
+      </form>
+      <Paper className={classes.paper}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Typography variant='h3'>{name}</Typography>
+          </Grid>
+          {dates && Object.entries(dates).map(([key, value]) => {
+            return <Grid item xs={12} key={key}>
+              <Typography>{key}: {value}</Typography>
+            </Grid>
+          })}
+        </Grid>
+      </Paper>
+    </Container>
+  </>
+}
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    marginBottom: 40
+  },
+  paper: {
+    marginTop: 40,
+    padding: 20
+  }
+}));
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+export async function getStaticProps() {
+  const rows = await getColumns();
+  const [headers, ...content] = rows
+
+  const result = content.reduce((acc, row) => {
+    const [key, ...values] = row
+    return { ...acc, [key]: values }
+  }, {})
+
+  const final = headers.reduce((acc, header, index) => {
+    if (header) {
+      return {
+        ...acc,
+        [header]: Object.entries(result).reduce((acc2, [key, value]) => ({ ...acc2, [key]: +value[index - 1] }), {})
+      }
+    }
+  }, {})
+
+  return {
+    props: {
+      data: final
+    },
+    revalidate: 1,
+  };
 }
